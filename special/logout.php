52c560511528778d21558c908120ce5bfe9dba68
R.G <61545240+Rix565@users.noreply.github.com>
2022-05-15 15:26:21 +0000

    Improvement: now tokens are deleted when you sign out

--- special/logout.php
+++ special/logout.php
@@ -2,5 +2,10 @@
 require_once "./utils/database.php";
 $dbClass = new Database;
 $dbClass->init_session();
+$conn = $dbClass->connect();
+$query = $conn->prepare("DELETE FROM `tokens` WHERE `token`=:token");
+$query->execute([
+    "token" => $_SESSION['token']
+]);
 session_unset();
 header("Location: /");
